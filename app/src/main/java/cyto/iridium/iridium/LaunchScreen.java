776260dd9d9d912f2f262d88b99637b8c4bae712
SammyDeAgent <rimamotostheking@gmail.com>
2021-08-18 11:24:24 +0800

    Implemented UI
    Navigation drawer and application launch screen.

---
+++ app/src/main/java/cyto/iridium/iridium/LaunchScreen.java
@@ -0,0 +1,31 @@
+package cyto.iridium.iridium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LaunchScreen extends AppCompatActivity {
+
+    Handler mHandler;
+    Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_screen);
+
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LaunchScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        mHandler.postDelayed(mRunnable,2000);
+    }
+}
