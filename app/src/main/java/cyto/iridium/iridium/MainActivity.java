4791818b6b64ea189a2c51248a834b887f84c2e6
SammyDeAgent <rimamotostheking@gmail.com>
2021-08-20 00:55:27 +0800

    Moved methods from activity to fragment

--- app/src/main/java/cyto/iridium/iridium/MainActivity.java
+++ app/src/main/java/cyto/iridium/iridium/MainActivity.java
@@ -30,14 +30,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import cyto.iridium.iridium.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-
-    private AccountAuthParams mAuthParam;
-    private AccountAuthService mAuthService;
-    private final String TAG = "Account";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,82 +61,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-        findViewById(R.id.hwid_signout).setOnClickListener(this);
-        findViewById(R.id.cancel_authorization).setOnClickListener(this);
-
-        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
-                .setProfile()
-                .setAuthorizationCode()
-                .setIdToken()
-                .setUid()
-                .setEmail()
-                .createParams();
-        mAuthService = AccountAuthManager.getService(MainActivity.this,mAuthParam);
-        mAuthService.getSignInIntent();
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.hwid_signout:
-                signOut();
-                break;
-            case R.id.cancel_authorization:
-                cancelAuthorization();
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void signOut() {
-        Task<Void> signOutTask = mAuthService.signOut();
-        signOutTask.addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Log.i(TAG, "signOut Success");
-                Toast.makeText(getApplicationContext(), "Signed Out Successfully", Toast.LENGTH_SHORT).show();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(Exception e) {
-                Log.i(TAG, "signOut fail");
-                Toast.makeText(getApplicationContext(), "Sign Out Procedure Failed", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Intent intent = new Intent(
-                this,
-                AuthWithAuthCodeActivity.class
-        );
-        startActivity(intent);
-        finish();
-    }
-
-    private void cancelAuthorization() {
-        Task<Void> task = mAuthService.cancelAuthorization();
-        task.addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Log.i(TAG, "cancelAuthorization success");
-                Toast.makeText(getApplicationContext(), "Authorization has been Revoked Successfully", Toast.LENGTH_SHORT).show();
-            }
-        });
-        task.addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(Exception e) {
-                Log.i(TAG, "cancelAuthorization failure：" + e.getClass().getSimpleName());
-                Toast.makeText(getApplicationContext(), "Authorization Revoked Procedure Failed" + e.getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Intent intent = new Intent(
-                this,
-                AuthWithAuthCodeActivity.class
-        );
-        startActivity(intent);
-        finish();
     }
 
     @Override
--- app/src/main/java/cyto/iridium/iridium/ui/home/HomeFragment.java
+++ app/src/main/java/cyto/iridium/iridium/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package cyto.iridium.iridium.ui.home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,22 +22,34 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 //import com.bumptech.glide.Glide;
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.support.account.AccountAuthManager;
+import com.huawei.hms.support.account.request.AccountAuthParams;
+import com.huawei.hms.support.account.request.AccountAuthParamsHelper;
 import com.huawei.hms.support.account.result.AuthAccount;
+import com.huawei.hms.support.account.service.AccountAuthService;
 import com.squareup.picasso.Picasso;
 
 import java.io.InputStream;
 
 import cyto.iridium.iridium.AuthWithAuthCodeActivity;
+import cyto.iridium.iridium.MainActivity;
 import cyto.iridium.iridium.R;
 import cyto.iridium.iridium.databinding.FragmentHomeBinding;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener{
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
     private String avatarURL;
     private String displayName;
+
+    private AccountAuthParams mAuthParam;
+    private AccountAuthService mAuthService;
+    private final String TAG = "Account";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -75,9 +89,84 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.hwid_signout:
+                signOut();
+                break;
+            case R.id.cancel_authorization:
+                cancelAuthorization();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void signOut() {
+        Task<Void> signOutTask = mAuthService.signOut();
+        signOutTask.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.i(TAG, "signOut Success");
+                Toast.makeText(getContext(), "Signed Out Successfully", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                Log.i(TAG, "signOut fail");
+                Toast.makeText(getContext(), "Sign Out Procedure Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Intent intent = new Intent(
+                this.getActivity(),
+                AuthWithAuthCodeActivity.class
+        );
+        startActivity(intent);
+        getActivity().finish();
+    }
+
+    private void cancelAuthorization() {
+        Task<Void> task = mAuthService.cancelAuthorization();
+        task.addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.i(TAG, "cancelAuthorization success");
+                Toast.makeText(getContext(), "Authorization has been Revoked Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                Log.i(TAG, "cancelAuthorization failure：" + e.getClass().getSimpleName());
+                Toast.makeText(getContext(), "Authorization Revoked Procedure Failed" + e.getClass().getSimpleName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Intent intent = new Intent(
+                this.getActivity(),
+                AuthWithAuthCodeActivity.class
+        );
+        startActivity(intent);
+        this.getActivity().finish();
+    }
+
 
     @Override
     public void onViewCreated(View view, @NonNull Bundle savedInstanceState){
+        binding.hwidSignout.setOnClickListener(this);
+        binding.cancelAuthorization.setOnClickListener(this);
+
+        mAuthParam = new AccountAuthParamsHelper(AccountAuthParams.DEFAULT_AUTH_REQUEST_PARAM)
+                .setProfile()
+                .setAuthorizationCode()
+                .setIdToken()
+                .setUid()
+                .setEmail()
+                .createParams();
+        mAuthService = AccountAuthManager.getService(this.getActivity(),mAuthParam);
+        mAuthService.getSignInIntent();
     }
 
     @Override
